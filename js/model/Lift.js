ec66e4eebfae7ad0d2fad1e5a425be40b24766f9
Luke0328 <curly0328@gmail.com>
2020-02-18 10:02:00 -0700

    Added documentation to Lift.js

--- js/model/Lift.js
+++ js/model/Lift.js
@@ -2,6 +2,18 @@
 
 /**
  * Lift is the general model for the two lifts seen in the simulation.
+ * There is an input lift and an output lift.
+ *
+ * Primary responsibilities:
+ *		- Keep track of the center position of each lift
+ *		- Keep track of the radius of each lift
+ *		- Keep track of the force exerted on the input lift or the force exerted by the output lift
+ *
+ * The output force changes according to the radii of the lifts and the input force.
+ * The lifts move up and down according to the forces. A greater force results in a greater
+ * displacement from the starting center position of each lift.
+ *
+ * For physics background information see https://en.wikipedia.org/wiki/Pascal%27s_law
  *
  * @author Luke Pan <curly0328@gmail.com>
  */
@@ -10,21 +22,21 @@ define( require => {
 	'use strict';
 
 	// modules
-	const assert = require( 'SIM_CORE/util/assert' );
-	const Property = require( 'SIM_CORE/util/Property' );
-	const Vector = require( 'SIM_CORE/util/Vector' );
+	// const assert = require( 'SIM_CORE/util/assert' );
+	// const Property = require( 'SIM_CORE/util/Property' );
+	// const Vector = require( 'SIM_CORE/util/Vector' );
 
 	class Lift {
 
 		/**
      * @param centerPosition - center position for the Lift
-		 * @param force - force exerted on or exerted by the Lift
-		 * @param radius - radius of the Lift surface
+		 * @param force - force exerted on or exerted by the Lift in newtons
+		 * @param radius - radius of the Lift surface in meters
 		 */
-		constructor( centerPosition, force, radius ) {
+		constructor( centerPosition ) {
 			this._centerPosition = centerPosition;
-			this._force = force;
-			this._radius = radius;
+			this._force = 0;
+			this._radius = 1;
 		}
 	}
 
