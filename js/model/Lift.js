23995dd88bc9a90a94b92596f54465899c3294dd
Luke0328 <curly0328@gmail.com>
2020-02-18 09:11:46 -0700

    Lint

--- js/model/Lift.js
+++ js/model/Lift.js
@@ -1,15 +1,15 @@
 // Copyright © 2020 Luke Pan. All rights reserved.
 
 /**
-* Lift is the general model for the two lifts seen in the simulation.
-*
-* @author Luke Pan <curly0328@gmail.com>
-*/
+ * Lift is the general model for the two lifts seen in the simulation.
+ *
+ * @author Luke Pan <curly0328@gmail.com>
+ */
 
 define( require => {
 	'use strict';
 
-	//modules
+	// modules
 	const assert = require( 'SIM_CORE/util/assert' );
 	const Property = require( 'SIM_CORE/util/Property' );
 	const Vector = require( 'SIM_CORE/util/Vector' );
@@ -17,10 +17,10 @@ define( require => {
 	class Lift {
 
 		/**
-    	* @param centerPosition - center position for the Lift
-		* @param force - force exerted on or exerted by the Lift
-		* @param radius - radius of the Lift surface
-		*/
+    	 * @param centerPosition - center position for the Lift
+		 * @param force - force exerted on or exerted by the Lift
+		 * @param radius - radius of the Lift surface
+		 */
 		constructor( centerPosition, force, radius ) {
 			this._centerPosition = centerPosition;
 			this._force = force;
