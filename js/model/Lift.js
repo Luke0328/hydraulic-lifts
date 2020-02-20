b47a1a6b3f2285b8622bb420ba1961c22f9285b4
Luke0328 <curly0328@gmail.com>
2020-02-19 21:13:00 -0700

    Added Properties to Lift.js

--- js/model/Lift.js
+++ js/model/Lift.js
@@ -6,7 +6,7 @@
  *
  * Primary responsibilities:
  *    - Keep track of the center position of each lift
- *    - Keep track of the radius of each lift
+ *    - Keep track of the surface radius of each lift
  *    - Keep track of the force exerted on the input lift or the force exerted by the output lift
  *
  * The output force changes according to the radii of the lifts and the input force.
@@ -23,20 +23,51 @@ define( require => {
 
   // modules
   // const assert = require( 'SIM_CORE/util/assert' );
-  // const Property = require( 'SIM_CORE/util/Property' );
-  // const Vector = require( 'SIM_CORE/util/Vector' );
+  const Property = require( 'SIM_CORE/util/Property' );
+  const Vector = require( 'SIM_CORE/util/Vector' );
 
   class Lift {
 
     /**
-     * @param centerPosition - center position for the Lift
-     * @param force - force exerted on or exerted by the Lift in newtons
-     * @param radius - radius of the Lift surface in meters
+     * @param {Vector} initialCenterPosition - center position for the Lift
+     * @param {object} [options] - controls Lift properties
      */
-    constructor( centerPosition ) {
-      this._centerPosition = centerPosition;
-      this._force = 0;
-      this._radius = 1;
+    constructor( initialCenterPosition, options ) {
+
+      options = {
+
+        initialForce = 0, // {number} - initial force exerted on or exerted by Lift, in Newtons
+
+        initialRadius = 1, // {number} - initial surface radius of Lift, in Meters
+
+        // rewrite options such that it overrides the defaults above if provided.
+        ...options
+      }
+
+      // @public (read-only) centerPositionProperty - Property of the position of the Lift center
+      this.centerPositionProperty = new Property( initialCenterPosition, { type: Vector } );
+
+      // @public (read-only) forceProperty - Property of the force on or from the Lift
+      this.forceProperty = new Property( options.initialForce, {
+        type: 'number',
+        isValidValue: value => value >= 0 // force must be greater than or equal to 0
+      } )
+
+      // @public (read-only) radiusProperty - Property of the surface radius of the Lift
+      this.radiusProperty = new Property( options.initialRadius, {
+        type: 'number',
+        isValidValue: value => value > 0 // radius must be greater than 0
+      } );
+    }
+
+    /**
+     * Resets the Lift properties to their initial values
+     * @public
+     */
+    reset() {
+      this.centerPositionProperty.reset();
+      this.forceProperty.reset();
+      this.radiusProperty.reset();
     }
   }
 
