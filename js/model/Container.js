466ca0e8d563fdb4590ee3c7a3ba7c2389be9f84
Luke0328 <curly0328@gmail.com>
2020-02-21 10:07:23 -0700

    Added to Container.js

--- js/model/Container.js
+++ js/model/Container.js
@@ -12,21 +12,28 @@ define( require => {
 
   // modules
   // const assert = require( 'SIM_CORE/util/assert' );
-  // const Property = require( 'SIM_CORE/util/Property' );
+  const Property = require( 'SIM_CORE/util/Property' );
   const Lift = require( 'HYDRAULIC_LIFTS/model/Lift' );
   const Vector = require( 'SIM_CORE/util/Vector' );
 
   // constants
   const INITIAL_INPUT_LIFT_CENTER = new Vector( 1, 1 );
-  const INITIAL_OUTPUT_LIFT_CENTER = new Vector( 10, 1 );
+  const INITIAL_OUTPUT_LIFT_CENTER = new Vector( 11, 1 );
+  const CONTAINER_CENTER_POSITION = new Vector( 5, 0 );
 
   class Container {
 
     constructor() {
 
+      this.inputOpeningProperty = new Property ();
+
+      this.outputOpeningProperty = new Property ();
+
       this.inputLift = new Lift( INITIAL_INPUT_LIFT_CENTER );
 
       this.outputLift = new Lift( INITIAL_OUTPUT_LIFT_CENTER );
+
+      this.containerCenterPosition = CONTAINER_CENTER_POSITION;
     }
 
   }
