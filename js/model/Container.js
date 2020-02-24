866d6ee1241a30227bd6e3c7021b850184000574
Luke0328 <curly0328@gmail.com>
2020-02-23 21:51:23 -0700

    Added to Container.js

--- js/model/Container.js
+++ js/model/Container.js
@@ -20,19 +20,27 @@ define( require => {
   const INITIAL_INPUT_LIFT_CENTER = new Vector( 1, 1 );
   const INITIAL_OUTPUT_LIFT_CENTER = new Vector( 11, 1 );
   const CONTAINER_CENTER_POSITION = new Vector( 5, 0 );
+  const OPENING_GAP = 0.05;
 
   class Container {
 
     constructor() {
 
-      this.inputOpeningWidthProperty = new Property();
+      // @public (read-only) inputOpeningWidthProperty - Property of the width of the input opening
+      this.inputOpeningWidthProperty = new Property( INITIAL_INPUT_LIFT_CENTER * 2 + OPENING_GAP,
+      { type: 'number' } );
 
-      this.outputOpeningWidthProperty = new Property();
+      // @public (read-only) outputOpeningWidthProperty - Property of the width of the output opening
+      this.outputOpeningWidthProperty = new Property( INITIAL_OUTPUT_LIFT_CENTER * 2 + OPENING_GAP,
+      { type: 'number' } );
 
-      this.inputLift = new Lift( INITIAL_INPUT_LIFT_CENTER );
+      // @public (read-only) {Lift} - Input Lift object
+      this.inputLift = new Lift( INITIAL_INPUT_LIFT_CENTER, { type: Vector } );
 
-      this.outputLift = new Lift( INITIAL_OUTPUT_LIFT_CENTER );
+      // @public (read-only) {Lift} - Output Lift object
+      this.outputLift = new Lift( INITIAL_OUTPUT_LIFT_CENTER, { type: Vector } );
 
+      // @public (read-only) containerCenterPosition - Center position of the container (constant)
       this.containerCenterPosition = CONTAINER_CENTER_POSITION;
     }
 
