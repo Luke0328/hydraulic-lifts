91c6e992ad334876bc8e670307a6ae7b5337cf7c
Luke0328 <curly0328@gmail.com>
2020-02-26 20:20:47 -0700

    Created ContainerNode.js

---
+++ js/view/ContainerNode.js
@@ -0,0 +1,23 @@
+// Copyright © 2020 Luke Pan. All rights reserved.
+
+/**
+ *
+ * @author Luke Pan <curly0328@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  // modules
+  // const assert = require( 'SIM_CORE/util/assert' );
+  // const Lift = require( 'HYDRAULIC_LIFTS/model/Lift' );
+  // const Property = require( 'SIM_CORE/util/Property' );
+  const SVGNode = require( 'SIM_CORE/scenery/SVGNode' );
+
+  class ContainerNode extends SVGNode {
+
+
+  }
+
+  return ContainerNode;
+} );
