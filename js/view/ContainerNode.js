0bc4a2fefa5967e6facce5938ba8e7d3d0d18f40
Luke0328 <curly0328@gmail.com>
2020-02-27 10:35:39 -0700

    Added to ContainerNode.js

--- js/view/ContainerNode.js
+++ js/view/ContainerNode.js
@@ -10,19 +10,32 @@ define( require => {
 
   // modules
   // const assert = require( 'SIM_CORE/util/assert' );
-  // const Lift = require( 'HYDRAULIC_LIFTS/model/Lift' );
+  const LiftNode = require( 'HYDRAULIC_LIFTS/view/LiftNode' );
   // const Property = require( 'SIM_CORE/util/Property' );
   const SVGNode = require( 'SIM_CORE/scenery/SVGNode' );
 
   class ContainerNode extends SVGNode {
 
-    constructor( container,  modelViewTransform, options) {
+    constructor( container, modelViewTransform, options ) {
 
       options = { ...options };
 
-      super ( options );
-    }
-  }
+      super( options );
 
+      // Create the input lift node
+      const inputLiftNode = new LiftNode( container.inputLift, modelViewTransform );
+
+      // Create the output lift node
+      const outputLiftNode = new LiftNode( container.outputLift, modelViewTransform );
+
+      const containerCenterRectangle = new Rectangle( {
+        width: 400,
+        height 300,
+        stroke: 'black',
+        fill: 'white',
+        strokewidth: 1
+       } );
+  }
+  }
   return ContainerNode;
 } );
