b5e5675feaa74a755177212b597b0689bd1825fd
Luke0328 <curly0328@gmail.com>
2020-03-01 22:24:37 -0700

    Laid out ControlPanel.js

---
+++ js/view/ControlPanel.js
@@ -0,0 +1,44 @@
+// Copyright © 2020 Luke Pan. All rights reserved.
+
+/**
+ *
+ * @author Luke Pan <curly0328@gmail.com>
+ */
+
+define( require => {
+  'use strict';
+
+  const Rectangle = require( 'SIM_CORE/scenery/Rectangle' );
+  const SliderNode = require( 'SIM_CORE/scenery/SliderNode' );
+  const SVGNode = require( 'SIM_CORE/scenery/SVGNode' );
+
+  class ControlPanel extends SVGNode {
+
+    constructor( options ) {
+      options = { ...options };
+      super( options );
+
+      // Create the background rectangle
+      const background = new Rectangle();
+
+      // Create the slider for the input force
+      const inputForceSlider = new SliderNode();
+
+      // Create the slider for the input radius
+      const inputRadiusSlider = new SliderNode();
+
+      // Create the slider for the output radius
+      const outputRadiusSlider = new SliderNode();
+
+      // Render the children in the correct z-layering
+      this.setchildren( [
+        background,
+        inputForceSlider,
+        inputRadiusSlider,
+        outputRadiusSlider
+       ] );
+    }
+  }
+
+  return ControlPanel;
+} );
